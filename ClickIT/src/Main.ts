aac472d9cf4a4ab87e95f2606987d909a0114bd9
maorbenami3 <maorbenami3@gmail.com>
2018-11-24 16:27:25 +0200

    change game config object

--- ClickIT/src/Main.ts
+++ ClickIT/src/Main.ts
@@ -1,66 +1,76 @@
-﻿/// <reference path="../ts_maps/phaser.d.ts" />
+﻿var Phaser = Phaser;
 module ClickIT {
     export class Main {
-        private mGame: Phaser.Game;
-        private mSprite1: Phaser.Sprite;
-        private mSprite2: Phaser.Sprite;
-        private mSprite3: Phaser.Sprite;
-        private mSprite4: Phaser.Sprite;
+        private mSprite1: /*Phaser.Sprite*/any;
+        private mWorldGravity: number = 100;
+        private mSpritesGroup: /*Phaser.Group*/ any;
+        private mCurrentNumber: number;
+        private mNextNumber: number;
+        private mCurrentVelocity: number;
+        private mSprites: Array<any>;
+        private mCubes: Array<Cube>;
+        private mContainer: HTMLDivElement;
+        private mScore: any;
+
         constructor() {
-            let aContainerDiv: HTMLDivElement = document.getElementById('content') as HTMLDivElement;
-            let aContainerBoundingRect: ClientRect = aContainerDiv.getBoundingClientRect();
-            this.mGame = new Phaser.Game(aContainerBoundingRect.width, aContainerBoundingRect.height, Phaser.AUTO, 'phaser-example', { preload: () => this.preload(), create: () => this.create(), render: () => this.render() });
+            this.mContainer = document.getElementById('content') as HTMLDivElement;
+            let aContainerBoundingRect: ClientRect = this.mContainer.getBoundingClientRect();
+            this.mCubes = new Array<Cube>();
+            Globals.game = new Phaser.Game(aContainerBoundingRect.width, aContainerBoundingRect.height, Phaser.AUTO, 'phaser-example', { preload: () => this.preload(), create: () => this.create(), update: () => this.update(),/*, render: () => this.render() */ });
+
         }
         //____________________________
         private preload() {
-            this.mGame.load.image('ilkke', 'www/img/ilkke.png');
+            Globals.game.load.image('cube', 'www/img/cube.png');
         }
         //____________________________
         private create() {
 
-            this.mGame.stage.backgroundColor = '#2d2d2d';
+            Globals.game.stage.backgroundColor = '#2d2d2d';
 
-            this.mGame.physics.startSystem(Phaser.Physics.ARCADE);
+            Globals.game.physics.startSystem(Phaser.Physics.ARCADE);
+            Globals.game.physics.setBoundsToWorld();
 
+            this.mSpritesGroup = Globals.game.add.group();
+            this.mSpritesGroup.enableBody = true;
+            this.mSpritesGroup.physicsBodyType = Phaser.Physics.ARCADE;
+            this.mSprites = new Array<any>();
+            let aContainerBoundingRect: ClientRect = this.mContainer.getBoundingClientRect();
+
+            for (let y = 0; y < 1; y++) {
+                for (let x = 0; x < 10; x++) {
+                    let aCube = this.mSpritesGroup.create(aContainerBoundingRect.left + x * 90, y * 50, 'cube');
+                    let aRandom: number = Math.floor(Math.random() * 100);
+                    let aCubeObject: Cube = new Cube(aCube, x, y, aRandom.toString());
+                    this.mSprites.push(aCubeObject.cube);
+                    this.mCubes.push(aCubeObject);
+                }
+            }
             //  Set the world (global) gravity
-            this.mGame.physics.arcade.gravity.y = 100;
-            //  Sprite 1 will use the World (global) gravity
-            this.mSprite1 = this.mGame.add.sprite(100, 96, 'ilkke');
-
-            //  Sprite 2 is set to ignore the global gravity and use its own value
-            this.mSprite2 = this.mGame.add.sprite(300, 96, 'ilkke');
-
-            //  Sprite 3 will use both the world gravity and its own gravityScale modifier
-            this.mSprite3 = this.mGame.add.sprite(500, 96, 'ilkke');
-
-            //  Sprite 4 will ignore all gravity
-            this.mSprite4 = this.mGame.add.sprite(700, 96, 'ilkke');
-
-            // Enable physics on those sprites
-            this.mGame.physics.enable([this.mSprite1, this.mSprite2, this.mSprite3, this.mSprite4], Phaser.Physics.ARCADE);
-
-            this.mSprite1.body.collideWorldBounds = true;
-            this.mSprite1.body.bounce.y = 0.8;
-
-            //this.mSprite2.body.collideWorldBounds = true;
-            ////this.mSprite2.body.bounce.y = 0.8;
-            ////this.mSprite2.body.gravity.y = 200;
-
-            // this.mSprite3.body.collideWorldBounds = true;
-            // this.mSprite3.body.bounce.y = 0.8;
-            // this.mSprite3.body.gravity.y = 50;
-
-            //this.mSprite4.body.allowGravity = false;
-
+            Globals.game.physics.arcade.gravity.y = this.mWorldGravity;
+        }
+        //____________________________
+        private update() {
+            for (let i: number = 0; i < this.mCubes.length; i++) {
+                let aSpriteCube = this.mCubes[i].cube;
+                this.mCubes[i].text.x = Math.floor(aSpriteCube.x + aSpriteCube.width / 2);
+                this.mCubes[i].text.y = Math.floor(aSpriteCube.y + aSpriteCube.height / 2);
+            }
         }
         //____________________________
         private render() {
-
-            this.mGame.debug.text('world gravity', this.mSprite1.x - 32, 64);
-            this.mGame.debug.text('local gravity', this.mSprite2.x - 32, 64);
-            this.mGame.debug.text('local / 2', this.mSprite3.x - 32, 64);
-            this.mGame.debug.text('no gravity', this.mSprite4.x - 32, 64);
+ 
         }
+        ////____________________________
+        //private alienOut(pCube) {
+
+        //    //  Move the cube to the top of the screen again
+        //    pCube.reset(pCube.x, 0);
+
+        //    //  And give it a new random velocity
+        //    pCube.body.velocity.y = 50 + Math.random() * 200;
+
+        //}
         //____________________________
     }
 }
